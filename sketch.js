29d8677a0b647eae1447a201adb0143e0eba91d7
Yuan Hua <yh2876@columbia.edu>
2018-01-24 15:10:21 -0500

    A01 HelloWorld

--- sketch.js
+++ sketch.js
@@ -3,22 +3,32 @@ function setup() {
 }
 
 function draw() {
+  // WEEK1: 1 WORD, 2 SHAPES, 3 COLORS
   background('yellow');
   // see https://p5js.org/reference/#/p5/fill
   noStroke()
   fill(10,150,255);
   // ellipse(locationX, locationY, width, height)
   // see https://p5js.org/reference/#/p5/ellipse
-  ellipse(320,100,50,50);
+  ellipse(width/2,height/2,50,50);
   // rect(locationX, locationY, width, height)
   // see https://p5js.org/reference/#/p5/rect
-  rect(280,150,80,5);
+  rect(width/2-40,height/2+40,80,5);
+
+  // WEEK2: ADD TIME
+  // see https://p5js.org/reference/#/p5/second
+  // see https://p5js.org/reference/#/p5/rotate
+  var s = second();
+  translate(width/2,height/2+15)
+  rotate(PI * s / 30);
   strokeWeight(1)
   stroke(50)
   noFill()
-  rect(250,50,140,140,50);
+  // rectangle radius: top-left, top-right, bottom-right, bottom-left
+  // when rotate, locationX,Y calculated from the translate location
+  rect(-70,-70,140,140,20,20,20,50);
   // see https://p5js.org/reference/#/p5/text
   textSize(20);
   fill(50,50,100);
-  text('Hi! Yuan is trying to code!', 240,300);
+  text('Hi! Yuan is trying to code!',-120,110);
 }
