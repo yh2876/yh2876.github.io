2831049b17e9b6b0093c9c8f8b3eda0a0b6f4581
Yuan Hua <yh2876@columbia.edu>
2018-03-07 22:47:02 -0500

    assignment 4.2 interactive

--- Assignment_04/interactive.js
+++ Assignment_04/interactive.js
@@ -1,118 +1,128 @@
-var headlines = [];
-var abstract = [];
-var section = [];
-var maxHeadLen, minHeadLen;
+// data variables
+var table;
+var park=[];
+var crime=[];
+var borough=[];
+var murder=[];
+var rape=[];
+var robbery=[];
+var felonyAssault=[];
+var burglary=[];
+var grandLarceny=[];
+var grandLarceny_motor=[];
 
-function preload() {
 
-  // Assemble url for API call
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  var apikey = "1c801111dd364dd3ae6e7dbc08742f58"; // see: https://developer.nytimes.com
-  url += "?api-key=" + apikey;
+// drawing constants
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+var margin = 40;
+var recHeight = 20;
+var textSize = 12;
 
-  response = loadJSON(url);
-  // loadJSON() is asynchronous, but calling it inside preload() guarantees
-  // we'll have a response before setup() and draw() is run.
+
+function preload(){ // display "Loading..." on the screen so we see something's happening
+  table = loadTable('NYC_ParkCrime_TotalCount_2017.csv', 'csv', 'header');
+  // console.log(table);
 }
 
-function setup() {
-  createCanvas(640, 800);
-  background(240);
 
-  textFont('Helvetica');
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  loadData();
+  noStroke();
+
+  input = createInput();
+  input.position(30,60);
+
+  button = createButton('SEARCH THIS PARK');
+  button.position(180, 60);
+  button.mousePressed(search);
+}
+
+function search(){
+  var keyword = input.value();
+  // text(keyword, 400,75);
+  // console.log(keyword);
+  var index = park.indexOf(keyword);
   textSize(12);
-  textAlign(LEFT);
-
-  extractHeadlines();
-  extractAbstract();
-  extractSection();
+  fill(255);
+  // console.log(index);
+  if (index!=-1){
+    text("CRIME NUMBER IN "+keyword+" is "+crime[index]+".", 350,75);
+  } else {
+    text("Failed to find "+keyword+" ...", 350, 75);
+  }
+  input.value('');
 }
 
-function draw() {
-  background(240);
+// function draw(){
+//   fill(255);
+//   textSize(14);
+//   text("SEARCH A PARK", 30,75);
+// }
 
-  var lineheight = 24;
-  var margin = 40;
-  translate(margin, margin);
 
-  for (var i = 0; i < headlines.length; i++) { // i++ = i+1, i-- = i-1
-    // draw abstracts and url
-    if(i < 30) {
-      if (mouseX >= 40 && mouseX-40-20<textWidth(headlines[i]) && mouseY-40 >= (i-1)*lineheight && mouseY-40 < i*lineheight) {
-          // change the color of the circle of the headlie
-		  var circleRadius = map(headlines[i].length,minHeadLen, maxHeadLen, margin, width-margin*2);
-		  fill(200);
-		  noStroke();
-		  ellipse(i*12,i*lineheight,circleRadius/8,circleRadius/8);
-      }
+// set excute interval
+setInterval(function draw(){ // change the displayed park names every 3 seconds
+  background(0);
+
+  // pick a random starting row of 100 park names
+  var i = Math.floor((Math.random()*1152-100)+1);
+  // console.log(i);
+
+  var textHeight = 30;
+  var textSpace = 30;
+  var row = int(windowHeight/30)-3; // determine rows displayed by windowHeight
+  // console.log(row);
+
+  // label
+  textSize(20);
+  fill('255');
+  text('NYC PARK CRIME (JAN - SEP)', 30,40);
+  textSize(14);
+  fill('red');
+  text("There is at least one crime.", 380, 40);
+  fill(170);
+  text("There is no crime.", 600, 40);
+  stroke(255);
+  line(30,50,windowWidth-30,50);
+
+  for (var j=0; j<=row; j++){ // ++ = +1 
+    noStroke();
+    var textX_1 = Math.floor((Math.random()*((windowWidth-60)/2-textWidth(park[i+j])))+0);
+    var textX_2 = Math.floor((Math.random()*((windowWidth-60)/2-textWidth(park[i+j*2])))+0)+(windowWidth-60)/2;
+    // console.log(textX);
+    var textY = (j+2)*30+40;
+    if (crime[i+j]!=0) {
+      fill('red');
+      textSize(16);
+    } else {
+      fill(170);
+      textSize(12);
     }
-  }
-
-  for (var i = 0; i < headlines.length; i++) { // i++ = i+1, i-- = i-1
-
-    // draw circle representing headline length
-    // normalize the length of headings
-    var circleRadius = map(headlines[i].length,minHeadLen, maxHeadLen, margin, width-margin*2);
-    noFill();
-    stroke(100);
-    if (i<30) {ellipse(i*12,i*lineheight,circleRadius/8,circleRadius/8);}
-
-    // draw headline
-    if(i < 30) { // show only 30 rows
-      noStroke();
-      fill(193,48,108);
-      text(i+1,0,i*lineheight); // show row number
-      text(headlines[i], 20, i*lineheight);
+    text(park[i+j], textX_1, textY);
+    // console.log(park[i+j]);
+    if (crime[i+j*2]!=0) {
+      fill('red');
+      textSize(16);
+    } else {
+      fill(170);
+      textSize(12);
     }
+    text(park[i+j*2], textX_2, textY);
   }
-
-  for (var i = 0; i < headlines.length; i++) { // i++ = i+1, i-- = i-1
-    // draw abstracts and url
-    if(i < 30) {
-      if (mouseX >= 40 && mouseX-40-20<textWidth(headlines[i]) && mouseY-40 >= (i-1)*lineheight && mouseY-40 < i*lineheight) {
-          // create a rectangle with size changing with the length of the abstract
-          fill(100);
-          rect(mouseX-40-5,mouseY-40+12,width-mouseX-30+5,12+lineheight*(2+int(textWidth(abstract[i])/(width-mouseX-30))));
-          fill(255);
-          text('Abstract:  '+abstract[i],mouseX-40,mouseY-40+lineheight*2,width-mouseX-30);
-          fill('yellow')
-          text('Section:  '+section[i],mouseX-40,mouseY-40+lineheight,width-mouseX-30);
-      }
-    }
-  }
-}
+},2000); // 2000ms = 2sec
 
 
-function extractHeadlines() {
-
-  for (var i = 0; i < response.results.length; i++) {
-    var h = response.results[i].title;
-    append(headlines, h);
-
-    if (!maxHeadLen) {
-      maxHeadLen = h.length;
-    } else if (h.length > maxHeadLen) {
-      maxHeadLen = h.length;
-    }
-
-    if (!minHeadLen) {
-      minHeadLen = h.length;
-    } else if (h.length < minHeadLen) {
-      minHeadLen = h.length;
-    }
-  }
-}
-
-function extractAbstract() {
-  for (var i = 0; i < response.results.length; i++) {
-    var a = response.results[i].abstract;
-    append(abstract, a);
-  }
-}
-
-function extractSection() {
-  for (var i = 0; i < response.results.length; i++) {
-    var s = response.results[i].section;
-    append(section, s);
-  }
+function loadData(){
+  var borough = table.getColumn("BOROUGH");
+  park = table.getColumn("PARK");
+  // console.log(park)
+  crime = table.getColumn("TOTAL");
+  // murder = table.getColumn("MURDER");
+  // rape = table.getColumn("RAPE");
+  // robbery = table.getColumn("ROBBERY");
+  // felonyAssault = table.getColumn("FELONY ASSULT");
+  // burglary = table.getColumn("BURGLARY");
+  // grandLarceny = table.getColumn("GRAND LARENCY");
+  // grandLarceny_motor = table.getColumn("GRAND LARCENY OF MOTOR VEHICLE");
 }
